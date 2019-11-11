4a09a8345c08fb212abb5c7367dc941c47dd81f8
ktnliu <ktnliu@gmail.com>
2019-11-11 13:38:28 -0800

    gui for transactions test

---
+++ src/ca/ubc/cs304/ui/TransactionsWindow.java
@@ -0,0 +1,31 @@
+package ca.ubc.cs304.ui;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import ca.ubc.cs304.delegates.TerminalTransactionsDelegate;
+
+/**
+ * The class is only responsible for displaying and handling the login GUI. 
+ */
+public class LoginWindow extends JFrame implements ActionListener {
+
+    private JTextField input; 
+
+
+}
