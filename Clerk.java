56b60eb21d939422e57a0db92d7175632a3c9fbb
colenliu <49017626+colenliu@users.noreply.github.com>
2019-11-20 16:31:47 -0800

    Add files via upload
    - changed structure of project so it's more similar to the librarian/borrower git that I shared with group
    - created and moved 8 files inside SRC: Renting, Reservation, Vehicle, vehicleType, Branch, Clerk, Customer, customerType
    - put in starter code for all of Clerk, Vehicle, vehicleType

---
+++ Clerk.java
@@ -0,0 +1,73 @@
+import java.sql.Date;
+import java.sql.Time;
+
+public class Clerk {
+    private String customer = "customer";
+    private String superCustomer = "superCustomer";
+    //java.sql.Connection con = Connection.getInstance().getConnection();
+    private static Object[][] receipt = new Object[30][5];
+    private static int rentCount;
+
+    public Clerk() {
+        // auto-generated constructor stub
+    }
+
+    public String returnCustomerType(int intType) {
+        String type = null;
+
+        return type;
+        //todo
+    }
+
+    public boolean addCustomer(int cellphone, String name, String address, int dlicense) {
+        return false;
+        //todo
+    }
+
+    public boolean rentWithReservation(int confno, int cellphone) {
+        return false;
+        //todo
+    }
+
+    public boolean rentWithoutReservation(String location, String type, Date fromdate, Time fromtime,
+                                          Date todate, Date totime) {
+        return false;
+        //todo
+    }
+
+    public Object[][] getReceipt() {
+        return receipt;
+    }
+
+    public int getCount() {
+        return rentCount;
+    }
+
+    public String returnVehicle(String vehicles_license, Date date, Time time, int odometer, boolean gas) {
+        return null;
+        //todo
+    }
+
+    public Object[][] generateDailyRentalsReport() {
+        return null;
+        //todo
+    }
+
+    public Object[][] generateDailyRentalsReportBranch() {
+        return null;
+        //todo
+    }
+
+    public Object[][] generateDailyReturnsReport() {
+        return null;
+        //todo
+    }
+
+    public Object[][] generateDailyReturnsReportBranch() {
+        return null;
+        //todo
+    }
+
+
+
+}
---
+++ vehicleType.java
@@ -0,0 +1,41 @@
+public class vehicleType {
+
+    public vehicleType() {
+
+    }
+//    java.sql.Connection con = Connection.getInstance().getConnection();
+    public int getWRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getDRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getHRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getHiRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getWiRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getDiRate(String type) {
+        return 0;
+        //todo
+    }
+
+    public int getKRate(String type) {
+        return 0;
+        //todo
+    }
+}
