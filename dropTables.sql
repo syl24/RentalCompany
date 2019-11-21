20b586ba3e9594df859e52ec031cd40c9884b21b
syl24 <syl24@live.com>
2019-11-21 01:16:01 -0800

    CreateTable and DropTable tested and done
    Going to work on fixing insert rows

--- dropTables.sql
+++ dropTables.sql
@@ -1,8 +1,9 @@
-drop table timeperiod;
-drop table reservations;
-drop table rentals;
-drop table vehicles;
-drop table vehicletypes;
-drop table customers;
-drop table returns;
+drop table branch cascade constraints;
+drop table timeperiod cascade constraints;
+drop table reservations cascade constraints;
+drop table rentals cascade constraints;
+drop table vehicles cascade constraints;
+drop table vehicletypes cascade constraints;
+drop table customers cascade constraints;
+drop table returns cascade constraints;
 commit;
